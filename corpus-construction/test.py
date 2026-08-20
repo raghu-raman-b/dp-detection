96fb96aa2121c174d188c8a728f852fce0b1e8f9
raghu-raman-b <itsraghuraman@gmail.com>
2026-08-20 13:11:19 +0530

    clean up repo

--- corpus-construction/test.py
+++
@@ -1,9 +0,0 @@
-from google_play_scraper import search
-import config
-
-title = "Candy Crush Saga"
-
-hits = search(title, lang=config.REVIEW_LANG, country=config.MARKETS[1],
-                          n_hits=16)
-
-print([(r['title'], r['appId']) for r in hits])
